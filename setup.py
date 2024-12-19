1a8e22c7238698436210a900be9a9cf80df67228
“AngadSinghLamba” <angadsinghlamba@gmil.com>
2024-12-19 22:22:45 +0530

    Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Angad Singh lamba',
+    author_email= 'angadsinghlamba@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
